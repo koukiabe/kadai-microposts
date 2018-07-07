9fc197b90b4fcf143ef11b0065dae93e019c05ca
kouki.abe <giants.abechin@gmail.com>
2018-07-07 17:25:39 +0000

    re for heroku

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,6 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favoriting_users, through: :favorites, source: :user
 end
